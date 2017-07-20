ed829201221fc3962459b3038cf43d07b283681e
Yakoozafm <yakooza.fm@gmail.com>
2017-07-20 16:19:00 +0200

    2

---
+++ mozgo-teszt/mozgo-teszt/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace mozgo_teszt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.taszimozgasDataSet = new mozgo_teszt.taszimozgasDataSet();
+            this.iranyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.iranyTableAdapter = new mozgo_teszt.taszimozgasDataSetTableAdapters.iranyTableAdapter();
+            this.tableAdapterManager = new mozgo_teszt.taszimozgasDataSetTableAdapters.TableAdapterManager();
+            this.iranyBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.iranyBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.iranyDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.taszimozgasDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iranyBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iranyBindingNavigator)).BeginInit();
+            this.iranyBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iranyDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // taszimozgasDataSet
+            // 
+            this.taszimozgasDataSet.DataSetName = "taszimozgasDataSet";
+            this.taszimozgasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // iranyBindingSource
+            // 
+            this.iranyBindingSource.DataMember = "irany";
+            this.iranyBindingSource.DataSource = this.taszimozgasDataSet;
+            // 
+            // iranyTableAdapter
+            // 
+            this.iranyTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.eszkozTableAdapter = null;
+            this.tableAdapterManager.felelosTableAdapter = null;
+            this.tableAdapterManager.iranyTableAdapter = this.iranyTableAdapter;
+            this.tableAdapterManager.iskolaTableAdapter = null;
+            this.tableAdapterManager.megnevezesTableAdapter = null;
+            this.tableAdapterManager.mozgasTableAdapter = null;
+            this.tableAdapterManager.oprendszerTableAdapter = null;
+            this.tableAdapterManager.tarhelyTableAdapter = null;
+            this.tableAdapterManager.technologiaTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = mozgo_teszt.taszimozgasDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // iranyBindingNavigator
+            // 
+            this.iranyBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.iranyBindingNavigator.BindingSource = this.iranyBindingSource;
+            this.iranyBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.iranyBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.iranyBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.iranyBindingNavigatorSaveItem});
+            this.iranyBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.iranyBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.iranyBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.iranyBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.iranyBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.iranyBindingNavigator.Name = "iranyBindingNavigator";
+            this.iranyBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.iranyBindingNavigator.Size = new System.Drawing.Size(1108, 25);
+            this.iranyBindingNavigator.TabIndex = 0;
+            this.iranyBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // iranyBindingNavigatorSaveItem
+            // 
+            this.iranyBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.iranyBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("iranyBindingNavigatorSaveItem.Image")));
+            this.iranyBindingNavigatorSaveItem.Name = "iranyBindingNavigatorSaveItem";
+            this.iranyBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.iranyBindingNavigatorSaveItem.Text = "Save Data";
+            this.iranyBindingNavigatorSaveItem.Click += new System.EventHandler(this.iranyBindingNavigatorSaveItem_Click);
+            // 
+            // iranyDataGridView
+            // 
+            this.iranyDataGridView.AutoGenerateColumns = false;
+            this.iranyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.iranyDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.iranyDataGridView.DataSource = this.iranyBindingSource;
+            this.iranyDataGridView.Location = new System.Drawing.Point(78, 45);
+            this.iranyDataGridView.Name = "iranyDataGridView";
+            this.iranyDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.iranyDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Nev";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Nev";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 631);
+            this.Controls.Add(this.iranyDataGridView);
+            this.Controls.Add(this.iranyBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.taszimozgasDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iranyBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iranyBindingNavigator)).EndInit();
+            this.iranyBindingNavigator.ResumeLayout(false);
+            this.iranyBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iranyDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private taszimozgasDataSet taszimozgasDataSet;
+        private System.Windows.Forms.BindingSource iranyBindingSource;
+        private taszimozgasDataSetTableAdapters.iranyTableAdapter iranyTableAdapter;
+        private taszimozgasDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator iranyBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton iranyBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView iranyDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    }
+}
+
---
+++ mozgo-teszt/mozgo-teszt/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mozgo_teszt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void iranyBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.iranyBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.taszimozgasDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'taszimozgasDataSet.irany' table. You can move, or remove it, as needed.
+            this.iranyTableAdapter.Fill(this.taszimozgasDataSet.irany);
+
+        }
+    }
+}
